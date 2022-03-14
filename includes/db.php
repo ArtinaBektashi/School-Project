bc12ff0b139eb24ac9a4f4c0811554f9259cf985
Artina Bektashi <ab51034@ubt-uni.net>
2022-03-14 17:47:33 +0100

    init

---
+++ includes/db.php
@@ -0,0 +1,12 @@
+<?php 
+
+$conn = new mysqli('localhost', 'root', '', 'skincare_db');
+
+    if($conn->connect_error) {
+        die('Connection failed: ' . $conn->connect_error);
+    };
+
+
+
+
+?>
